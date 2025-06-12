f9864fc8193fc198a3d7fa3cf3d5cd09962047d1
cc <claude.cockfield@bison.howard.edu>
2025-06-12 19:36:16 -0400

    anontion.sh fixes part-n

--- sql/anontion.sh
+++ sql/anontion.sh
@@ -8,7 +8,7 @@ then
   exit 1
 fi
 
-mysql -u root -pNNNNN -h 127.0.0.1 anontion_live < ${MYSQL_CONFIGFILE}
+mysql -u root -pNNNNN -h 127.0.0.1 asterisk_live < ${MYSQL_CONFIGFILE}
 
 if [[ "$?" != "0" ]]
 then
