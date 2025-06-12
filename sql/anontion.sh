d5f61ecb24bcb9a9f74d3d0ccd5ebf6a52235f48
cc <claude.cockfield@bison.howard.edu>
2025-06-12 12:10:21 -0400

    split compose. added anontion data population.

---
+++ sql/anontion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MYSQL_CONFIGFILE=/usr/local/src/asterisk/contrib/realtime/mysql/mysql_config.sql
+
+if [[ ! -f "${MYSQL_CONFIGFILE}" ]]
+then
+  echn "mysql_config file missing ${MYSQL_CONFIGFILE} "
+  exit 1
+fi
+
+mysql -u root -pNNNN -h 127.0.0.1 anontion_live < ${MYSQL_CONFIGFILE}
+
+if [[ "$?" != "0" ]]
+then
+  echn "mysql_config update failed ${MYSQL_CONFIGFILE} "
+  exit 1
+fi
+
