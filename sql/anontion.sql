3e8216d70c491b1826804ce6b6c8a7c0adc36c29
cc <claude.cockfield@bison.howard.edu>
2025-06-13 10:45:29 -0400

    ps_transports population, comment out extensions, mailbox and pjsip static conf

---
+++ sql/anontion.sql
@@ -0,0 +1,4 @@
+INSERT INTO ps_transports (id, type, protocol, bind, local_net,
+                           external_media_address, external_signaling_address)
+VALUES ('transport-id', 'transport', 'udp', '0.0.0.0',
+        '10.0.0.0/24', '10.0.0.99', '10.0.0.99');
