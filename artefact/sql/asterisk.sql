15aa09725c94d5dace4b3ca08d1c70eb6f6b2f46
cc <claude.cockfield@bison.howard.edu>
2025-07-08 02:52:17 -0400

    added test1 endpoint with realistic names

--- artefact/sql/asterisk.sql
+++ artefact/sql/asterisk.sql
@@ -43,9 +43,22 @@ VALUES ('8000', 1, 'yes', 30, 'yes');
 INSERT INTO ps_auths (id, auth_type, username, password)
 VALUES ('8000', 'userpass', '8000', 'password');
 
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+                          trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
+                          send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
+VALUES ('NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'transport-id', 'NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'BKPKBkHMq0ZBvutaIX6ngjQs7urTAgeKCjl/XcXOal3lqb9vBB9JHCcH7cIetYH/w0t7ChsXGMrV6S/DWlvxMYw=', 'external', 'all',
+        'h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
+        'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <test1>');
+
+INSERT INTO ps_aors (id, max_contacts, remove_existing, qualify_frequency, support_path)
+VALUES ('NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 1, 'yes', 30, 'yes');
+
+INSERT INTO ps_auths (id, auth_type, username, password)
+VALUES ('BKPKBkHMq0ZBvutaIX6ngjQs7urTAgeKCjl/XcXOal3lqb9vBB9JHCcH7cIetYH/w0t7ChsXGMrV6S/DWlvxMYw=', 'userpass', '8000', 'password');
+
 COMMIT;
 END; //
 
 DELIMITER ;
 
-/* CALL insert_pjsip_data(); */
+CALL insert_pjsip_data();
