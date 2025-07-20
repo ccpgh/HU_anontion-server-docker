8a93c61b95c5d88bfb5dc8338b9bdf6bd0e27250
cc <claude.cockfield@bison.howard.edu>
2025-07-20 02:12:51 -0400

    moved opus to front of sql options

--- artefact/sql/asterisk.sql
+++ artefact/sql/asterisk.sql
@@ -21,7 +21,7 @@ INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, d
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
 VALUES ('8001', 'transport-id', '8001', '8001', 'external', 'all',
-        'h264,g729,gsm,opus,speex', 'no', 'yes', 'rfc4733', 'yes',
+        'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8001>');
 
 INSERT INTO ps_aors (id, max_contacts, remove_existing, qualify_frequency, support_path)
@@ -34,7 +34,7 @@ INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, d
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
 VALUES ('8000', 'transport-id', '8000', '8000', 'external', 'all',
-        'h264,g729,gsm,opus,speex', 'no', 'yes', 'rfc4733', 'yes',
+        'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8000>');
 
 INSERT INTO ps_aors (id, max_contacts, remove_existing, qualify_frequency, support_path)
@@ -47,7 +47,7 @@ INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, d
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
 VALUES ('NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'transport-id', 'NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'BKPKBkHMq0ZBvutaIX6ngjQs7urTAgeKCjl/XcXOal3lqb9vBB9JHCcH7cIetYH/w0t7ChsXGMrV6S/DWlvxMYw=', 'external', 'all',
-        'h264,g729,gsm,opus,speex', 'no', 'yes', 'rfc4733', 'yes',
+        'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <test1>');
 
 INSERT INTO ps_aors (id, max_contacts, remove_existing, qualify_frequency, support_path)
