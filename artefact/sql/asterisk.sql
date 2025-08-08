8fa1779fe8ae281b6a976184d52d105143eec98e
cc <claude.cockfield@bison.howard.edu>
2025-08-08 12:47:02 -0400

    final fix of asterisk sql for message context, i hope

--- artefact/sql/asterisk.sql
+++ artefact/sql/asterisk.sql
@@ -18,10 +18,10 @@ END;
 START TRANSACTION;
 
 
-INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, message_context, disallow, allow, direct_media,
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
-VALUES ('8000', 'transport-id', '8000', '8000', 'external', 'all',
+VALUES ('8000', 'transport-id', '8000', '8000', 'external', 'messages', 'all',
         'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8000>');
 
@@ -32,10 +32,10 @@ INSERT INTO ps_auths (id, auth_type, username, password)
 VALUES ('8000', 'userpass', '8000', 'password');
 
 
-INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, message_context, disallow, allow, direct_media,
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
-VALUES ('8001', 'transport-id', '8001', '8001', 'external', 'all',
+VALUES ('8001', 'transport-id', '8001', '8001', 'external', 'messages', 'all',
         'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8001>');
 
@@ -46,10 +46,10 @@ INSERT INTO ps_auths (id, auth_type, username, password)
 VALUES ('8001', 'userpass', '8001', 'password');
 
 
-INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, message_context, disallow, allow, direct_media,
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
-VALUES ('8002', 'transport-id', '8002', '8002', 'external', 'all',
+VALUES ('8002', 'transport-id', '8002', '8002', 'external', 'messages', 'all',
         'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8002>');
 
@@ -61,10 +61,10 @@ VALUES ('8002', 'userpass', '8002', 'password');
 
 
 
-INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, message_context, disallow, allow, direct_media,
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
-VALUES ('8003', 'transport-id', '8003', '8003', 'external', 'all',
+VALUES ('8003', 'transport-id', '8003', '8003', 'external', 'messages', 'all',
         'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <8003>');
 
@@ -76,10 +76,10 @@ VALUES ('8003', 'userpass', '8003', 'password');
 
 
 
-INSERT INTO ps_endpoints (id, transport, aors, auth, context, disallow, allow, direct_media,
+INSERT INTO ps_endpoints (id, transport, aors, auth, context, message_context, disallow, allow, direct_media,
                           trust_id_outbound, dtmf_mode, force_rport, rtp_symmetric,
                           send_rpid, ice_support, tos_video, cos_video, allow_subscribe, callerid)
-VALUES ('NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'transport-id', 'NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'BKPKBkHMq0ZBvutaIX6ngjQs7urTAgeKCjl/XcXOal3lqb9vBB9JHCcH7cIetYH/w0t7ChsXGMrV6S/DWlvxMYw=', 'external', 'all',
+VALUES ('NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'transport-id', 'NEH3pWZLb-Y6vobsYxx0B6U12p6XchXPgGZnbATL', 'BKPKBkHMq0ZBvutaIX6ngjQs7urTAgeKCjl/XcXOal3lqb9vBB9JHCcH7cIetYH/w0t7ChsXGMrV6S/DWlvxMYw=', 'external', 'messages', 'all',
         'opus,h264,g729,gsm', 'no', 'yes', 'rfc4733', 'yes',
         'yes', 'yes', 'yes', 'af41', 4, 'yes', 'User <test1>');
 
