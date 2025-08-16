2a0ee732b8bebcfa2207b67e9da8441341de02ae
cc <claude.cockfield@bison.howard.edu>
2025-08-16 03:09:08 -0400

    changes for self signed cert generation

--- artefact/sh/certificate.sh
+++ artefact/sh/certificate.sh
@@ -4,4 +4,8 @@ openssl genrsa -out asterisk.key 2048
 openssl req -new -key asterisk.key -out asterisk.csr -config asterisk.cnf
 openssl x509 -req -days 365 -in asterisk.csr -signkey asterisk.key -out asterisk.crt -extensions req_ext -extfile asterisk.cnf
 
+chown asterisk ./asterisk.*
+
+chmod 400 ./asterisk.*
+
 
