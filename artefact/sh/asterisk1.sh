4e501d8d3ea529f2d4d684b0d2ad764fb00533cd
cc <claude.cockfield@bison.howard.edu>
2025-09-04 11:19:33 -0400

    split database updates due to issues in prod build

--- artefact/sh/asterisk1.sh
+++ artefact/sh/asterisk1.sh
@@ -2,7 +2,6 @@
 
 MYSQL_CONFIGFILE=/usr/local/src/asterisk/contrib/realtime/mysql/mysql_config.sql
 MYSQL_PASSWORDFILE=/usr/local/src/sql/password
-MYSQL_DATAFILE=/usr/local/src/sql/asterisk.sql
 
 if [[ ! -f "${MYSQL_CONFIGFILE}" ]]
 then
@@ -17,12 +16,3 @@ then
   echo "mysql_config update failed ${MYSQL_CONFIGFILE}"
   exit 1
 fi
-
-mysql --defaults-extra-file=${MYSQL_PASSWORDFILE} -u anontion -h 127.0.0.1 anontion_live < ${MYSQL_DATAFILE}
-
-if [[ "$?" != "0" ]]
-then
-  echo "mysql_config update failed ${MYSQL_DATAFILE}"
-  exit 1
-fi
-
---
+++ artefact/sh/asterisk2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MYSQL_PASSWORDFILE=/usr/local/src/sql/password
+MYSQL_DATAFILE=/usr/local/src/sql/asterisk.sql
+
+if [[ ! -f "${MYSQL_DATAFILE}" ]]
+then
+  echo "mysql data file missing ${MYSQL_DATAFILE} "
+  exit 1
+fi
+
+mysql --defaults-extra-file=${MYSQL_PASSWORDFILE} -u anontion -h 127.0.0.1 anontion_live < ${MYSQL_DATAFILE}
+
+if [[ "$?" != "0" ]]
+then
+  echo "mysql data update failed ${MYSQL_DATAFILE}"
+  exit 1
+fi
+
